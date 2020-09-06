c2f788e82d0e11f185841dcf7a02a48bee1998bb
kumar-jitendra <jitendra.kumar@gmail.com>
2020-09-07 01:47:02 +0530

    5th commit, managed login and some ui improvements

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const Ticket = require('../models/ticket_model');
+const fs = require('fs');
+
+let cur;
+fs.readFile('currentUser.json', (err, data) => {
+    if (err) {
+        throw err;
+    }
+    cur = JSON.parse(data);
+});
 
 // DASHBOARD ROUTE SETUP
 router.get('/dashboard', async (req, res) => {
 
     try {
+
         const allTickets = await Ticket.find();
         //  res.send(allTickets);
-        res.render('dashboard', { allTickets: allTickets });
-        console.log("get request");
+        console.log(cur.curuser);
+        res.render('dashboard', { allTickets: allTickets, loggedInUser: cur });
 
     } catch (err) {
         res.send('Error' + err);
--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user_model');
 const Ticket = require('../models/ticket_model');
+const fs = require("fs"); 
 
 // LOGIN ROUTE SETUP
 router.get('/', (req, res) => {
@@ -19,27 +20,35 @@ router.post('/api/login', async (req, res) => {
         const allUsers = await User.find();
         let flag = false;
 
+        const currentUser = {
+            curuser : `${req.body.user}`,
+            curemp : `${req.body.empcode}`
+        }
+
+        fs.writeFile('currentUser.json', JSON.stringify(currentUser), () => {
+            console.log('current user saved into json');
+        })
+
+        
         allUsers.forEach(e => {
             if (e.empCode == req.body.empcode) {
                 flag = true;
             }
         });
-
+       
         if (!flag) {
             await user.save((error, result) => {
                 if (error) {
                     console.log(error);
                 }
-                else {
-                    console.log(result);
-                }
+                // else {
+                //     console.log(result);
+                // }
             });
         }
 
-        //  console.log(allUsers);
         res.redirect('/dashboard');
-        // res.render('dashboard', { allTickets: allTickets, userName : req.body.user });
-
+        
 
     } catch (error) {
         res.send(error);
